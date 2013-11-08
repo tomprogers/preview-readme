83a66ea5e5e5d64477e50d6fe3834a71f78e1626
hkk <hanse19@gmail.com>
2013-11-08 11:08:52 +0100

    Support more readme file name formats

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ var app      = connect()
 
 app.use(connect.static('public'));
 app.use(function(req, res) {
-  fs.readFile('Readme.md', {encoding: 'utf8'}, function(err, data) {
+  var readmeFiles = ["README", "README.md", "Readme.md", "README.markdown"]
+    , chosenReadme;
+
+  for (var i = 0; i < readmeFiles.length; ++i) {
+    if (fs.existsSync(readmeFiles[i])) {
+      chosenReadme = readmeFiles[i];
+      break;
+    }
+  }
+
+  if (!chosenReadme) {
+    console.log('Could not locate a readme file.');
+    return res.end('No readme file was found in this directory.');
+  }
+
+  fs.readFile(chosenReadme, {encoding: 'utf8'}, function(err, data) {
     if (err) throw err;
     renderer(data, token, function(err, rendered) {
       res.end(rendered);
     });
-  });
+  })
+
 });
 
 http.createServer(app).listen(5000, function() {
