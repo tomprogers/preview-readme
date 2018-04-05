bed0507b78fcc08a1661a41a15335dc4fef80306
Tom Rogers <tom@roguevendor.com>
2018-04-04 21:20:30 -0500

    expanded list of default files; minor style tweaks

--- index.js
+++ index.js
@@ -7,33 +7,39 @@ var app      = connect();
 var token    = process.env.README_PREVIEW_API_KEY || process.argv[2];
 
 var readmeFiles = [
-	"README",
-	"README.md",
-	"Readme.md",
-	"README.markdown"
+	'readme',
+	'README',
+	'Readme',
+	'readme.md',
+	'README.MD',
+	'Readme.md',
+	'readme.markdown',
+	'README.MARKDOWN',
+	'Readme.markdown'
 ];
 
 app.use(function(req, res) {
 	var chosenReadme;
 	
 	for(var i = 0; i < readmeFiles.length; ++i) {
-		if (fs.existsSync(readmeFiles[i])) {
+		if(fs.existsSync(readmeFiles[i])) {
 			chosenReadme = readmeFiles[i];
 			break;
 		}
 	}
 	
-	if (!chosenReadme) {
+	if(!chosenReadme) {
 		console.log('Could not locate a readme file.');
 		return res.end('No readme file was found in this directory.');
 	}
 	
-	fs.readFile(chosenReadme, {encoding: 'utf8'}, function(err, data) {
-		if (err) throw err;
+	fs.readFile(chosenReadme, { encoding: 'utf8' }, function(err, data) {
+		if(err) throw err;
+		
 		renderer(data, token, function(err, rendered) {
 			res.end(rendered);
 		});
-	})
+	});
 });
 
 http.createServer(app).listen(5000, function() {
